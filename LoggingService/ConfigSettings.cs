503334db5c035b4d8c928c9f56a9b4471d484899
Amit Chawla <acamit84@gmail.com>
2019-11-11 21:07:01 +0530

    add logging service

---
+++ LoggingService/ConfigSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace LoggingService
+{
+    public class ConfigSettings
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ LoggingService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace LoggingService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((webHostBuilderContext, configurationBuilder) =>
+            {
+                var hostingEnvironment = webHostBuilderContext.HostingEnvironment;
+                configurationBuilder.AddConfigServer(hostingEnvironment.EnvironmentName);
+            }).UseStartup<Startup>();
+    }
+}
